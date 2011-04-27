3f2792431d9eb3ab57b0648a13b34fe36e19cfa1
Brandon Dimcheff <bdimchef-git@wieldim.com>
2011-04-26 20:46:12 -0400

    implemented duo signing/verifying

--- lib/duo.rb
+++ lib/duo.rb
@@ -1,3 +1,45 @@
+require 'openssl'
+require 'base64'
+
 module Duo
-  # Your code goes here...
+  
+  # Sign a Duo request with skey, ikey, and the local username
+  def sign_request(skey, ikey, username)
+    exp = Time.now.to_i + 300
+
+    val = [username, ikey, exp].join('|')
+    
+    b64 = Base64.encode64(val).strip
+    cookie = "TX|#{b64}"
+
+    sig = hmac_sha1(skey, cookie)
+
+    [cookie, sig].join('|')
+  end
+
+  # Check a response from Duo with the skey.
+  def verify_response(skey, sig_response)
+    ts = Time.now.to_i
+    u_prefix, u_b64, u_sig = sig_response.to_s.split('|')
+    
+    sig = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'),
+      skey, [u_prefix, u_b64].join('|'))
+      
+    return nil if hmac_sha1(skey, sig) != hmac_sha1(skey, u_sig)
+    
+    return nil if u_prefix != 'AUTH'
+    
+    user, ikey, exp = Base64.decode64(u_b64).to_s.split('|')
+    
+    return nil if ts >= exp.to_i
+    
+    return user
+  end
+  
+  private
+  def hmac_sha1(key, data)
+    OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), key, data.to_s)
+  end
+  
+  extend self
 end
--- spec/duo_spec.rb
+++ spec/duo_spec.rb
@@ -1,7 +1,55 @@
 require 'spec_helper'
 
+IKEY = "DIXXXXXXXXXXXXXXXXXX"
+SKEY = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+
+USER = "testuser"
+
+INVALID_RESPONSE = "  |INVALID|SIG"
+EXPIRED_RESPONSE = "AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTMwMDE1Nzg3NA==|cb8f4d60ec7c261394cd5ee5a17e46ca7440d702"
+FUTURE_RESPONSE = "AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0Mw==|d20ad0d1e62d84b00a3e74ec201a5917e77b6aef"
+
+
 describe Duo do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  
+  context "signing" do
+    
+    it "doesn't fail spectacularly" do
+      lambda { Duo.sign_request(SKEY, IKEY, USER) }.should_not raise_error
+    end
+    pending "write tests, dug!!"
+  end
+  
+  context "verifying" do
+    it "verifies a valid response" do
+      future_user = Duo.verify_response(SKEY, FUTURE_RESPONSE)
+      future_user.should == USER
+    end
+    
+    it "rejects an invalid user" do
+      future_user = Duo.verify_response(SKEY, INVALID_RESPONSE)
+      future_user.should be_nil
+    end
+    
+    it "rejects an expired user" do
+      future_user = Duo.verify_response(SKEY, EXPIRED_RESPONSE)
+      future_user.should be_nil
+    end
+    
+    it "doesn't crash when nil" do
+      lambda { Duo.verify_response(SKEY, nil) }.should_not raise_error
+    end
+    
+    it "doesn't crash when nonsense" do
+      lambda { Duo.verify_response(SKEY, 'abceaouhsnt') }.should_not raise_error
+    end
+    
+    it "doesn't crash when pipey" do
+      lambda { Duo.verify_response(SKEY, '|') }.should_not raise_error
+    end
+    
+    it "doesn't crash when 7" do
+      lambda { Duo.verify_response(SKEY, 7) }.should_not raise_error
+    end
   end
 end
